da0e52de0efb9fe3898f39e061b6394124c4609e
Sagar Joshi <sagar.2.joshi@atos.net>
2019-02-22 19:05:43 +0530

    added custom http handler and custom http module samples

---
+++ CustomHandlerAndModule/CustomHandlerAndModule/App_Code/SagarCustomHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Web;
+using System.Web.Routing;
+
+namespace CustomHandlerAndModule.App_Code
+{
+    public class SagarCustomHandler : IHttpHandler
+    {
+        public RequestContext RequestContext { get; set; }
+
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+
+        public SagarCustomHandler()
+        { 
+        
+        }
+
+        //public SagarCustomHandler(RequestContext sagarreqcontext)
+        //{
+        //    RequestContext = sagarreqcontext;
+    
+        //}
+
+        public void ProcessRequest(HttpContext context)
+        {
+            //throw new NotImplementedException();
+            context.Response.Write("HI FROM SAGAR");
+        }
+    }
+}
---
+++ CustomHandlerAndModule/CustomHandlerAndModule/App_Code/SagarCustomHttpModule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Web;
+
+namespace CustomHandlerAndModule.App_Code
+{
+    public class SagarCustomHttpModule  : IHttpModule
+    {
+        private StreamWriter sw;
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += context_BeginRequest;
+        }
+
+        void context_BeginRequest(object sender, EventArgs e)
+        {
+            string filepath = @"C:\PrivateRepo\GitRepo\AssignmentsRepo\CustomHandlerAndModule\CustomHandlerAndModule\logger.txt";
+            //throw new NotImplementedException();
+            //if (!File.Exists(filepath))
+            //{
+            //    sw = new StreamWriter(filepath);
+            //}
+            //else
+            //    sw = File.AppendText(filepath);
+            //sw.WriteLine("User send request on : "+ DateTime.Now.ToShortDateString());
+
+        }
+    }
+}
