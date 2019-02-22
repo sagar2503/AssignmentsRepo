c41a9a1c3cb874a0f3272b7590395f685834c602
Sagar Joshi <sagar.2.joshi@atos.net>
2019-02-22 23:55:39 +0530

    out and ref keywords
    https://www.youtube.com/watch?v=lYdcY5zulXA

---
+++ RefVsOut/RefVsOut/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefVsOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://www.youtube.com/watch?v=lYdcY5zulXA
+
+            int passbyvalue = 10;
+            PassByValue(passbyvalue);
+            Console.WriteLine("PassByValue: "+ passbyvalue.ToString());
+
+            int passbyref = 10;
+            PassByRef(ref passbyref);
+            Console.WriteLine("PassByRef: " + passbyref.ToString());
+
+            int outParam ;
+            PassAsOutParam(out outParam);
+            Console.WriteLine("OutParam: " + outParam.ToString());
+            
+            Console.ReadLine();
+
+        }
+
+        static void PassByValue(int InsideVar)
+        {
+            InsideVar = InsideVar + 20;
+        }
+
+        static void PassByRef(ref int InsideVar)
+        {
+            InsideVar = InsideVar + 20;
+        }
+
+        static void PassAsOutParam(out int InsideVar)
+        {
+            InsideVar = 20;
+        }
+    }
+}
