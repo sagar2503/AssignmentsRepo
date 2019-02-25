31f572b94afe6307208dde1e93fcf5343917bf5d
Sagar Joshi <sagar.2.joshi@atos.net>
2019-02-25 23:51:17 +0530

    formatters
    https://www.youtube.com/watch?v=C3q9C6s0s7w&index=5&list=PL6n9fhu94yhW7yoUOGNOfHurUE6bpOO2b

--- ASPMVCWEbApiSample/ASPMVCWEbApiSample/App_Start/WebApiConfig.cs
+++ ASPMVCWEbApiSample/ASPMVCWEbApiSample/App_Start/WebApiConfig.cs
@@ -25,6 +25,9 @@ namespace ASPMVCWEbApiSample
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
