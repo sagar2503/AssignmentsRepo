b8c8cf3f85d6b755b2d1d483586dba24b130f556
Sagar Joshi <sagar.2.joshi@atos.net>
2019-02-26 00:15:44 +0530

    custommediatypeformatter

--- ASPMVCWEbApiSample/ASPMVCWEbApiSample/App_Start/WebApiConfig.cs
+++ ASPMVCWEbApiSample/ASPMVCWEbApiSample/App_Start/WebApiConfig.cs
@@ -2,12 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Net.Http.Headers;
 
 namespace ASPMVCWEbApiSample
 {
+
+    public class CustomJSonFormater : JsonMediaTypeFormatter
+    {
+        public CustomJSonFormater()
+        {
+            this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        }
+
+        public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+        {
+            base.SetDefaultContentHeaders(type, headers, mediaType);
+            headers.ContentType = new MediaTypeHeaderValue("application/json");
+        }
+    }
+
+
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
@@ -26,8 +44,11 @@ namespace ASPMVCWEbApiSample
                 defaults: new { id = RouteParameter.Optional }
             );
 
-            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
-            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.Add(new CustomJSonFormater());
+            //config.Formatters.Remove(config.Formatters.XmlFormatter);
+            //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+            //config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            //config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
