301e587d4eea68f6d6103379491e953d33b3bf41
Sagar Joshi <sagar.2.joshi@atos.net>
2019-02-26 14:59:11 +0530

    part5 : kudvenkat : asp.netmvc
    https://youtu.be/N6srbKfNcV4

---
+++ ASPMVCWebAppSample/ASPMVCWebAppSample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPMVCWebAppSample.Startup))]
+namespace ASPMVCWebAppSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
